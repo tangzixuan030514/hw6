09fcde4a9ca8ff992aa6e591dfdd5f27f769864a
tangzixuan030514 <tangzixuan0514@gmail.com>
2024-11-26 17:59:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+Highcharts.chart('container', {
+    chart: {
+        type: 'pie'
+    },
+    title: {
+        text: '2023來臺國籍人次統計'
+    },
+    tooltip: {
+        valueSuffix: ''
+    },
+    subtitle: {
+        text:
+        'Source:<a href="臺灣觀光署'
+    },
+    plotOptions: {
+        series: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: [{
+                enabled: true,
+                distance: 20
+            }, {
+                enabled: true,
+                distance: -40,
+                format: '{point.percentage:.1f}%',
+                style: {
+                    fontSize: '1.2em',
+                    textOutline: 'none',
+                    opacity: 0.7
+                },
+                filter: {
+                    operator: '>',
+                    property: 'percentage',
+                    value: 10
+                }
+            }]
+        }
+    },
+    series: [
+        {
+            name: '人數',
+            colorByPoint: true,
+            data: [
+                {
+                    name: '亞洲地區',
+                    y: 3971251,
+                    sliced: true,
+                    selected: true,
+                },
+                {
+                    name: '非洲地區',
+                    
+                    y: 9510
+                },
+                {
+                    name: '美洲地區',
+                    y: 670162
+                },
+                {
+                    name: '大洋洲地區',
+                    y: 113772
+                },
+                {
+                    name: '歐洲地區',
+                    y: 346091
+                },
+                {
+                    name: '其他未列明',
+                    y: 854
+                }
+            ]
+        }
+    ]
+});
